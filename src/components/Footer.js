130431993fd660f421285df4d88940217325b32d
mbulling <42184555+mbulling@users.noreply.github.com>
2022-02-28 16:30:44 -0500

    nice index page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
+import '../static/styles/master.css';
+
 const Footer = () => {
     return (
   
-        <div className="foot">CornellCensus, 2022.</div>
+        <div className="foot" >
+        <strong><p style={{marginBottom:"0", fontSize:"15px"}}>©CornellCensus, 2022.</p></strong>
+        <p style={{marginTop:"0", bottom:"-10px",  fontSize:"15px"}}>Managed by Cornell Data Science.</p>
+        </div>
+        
       )
     }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,7 +43,8 @@ function Home() {
         <Comp2 />
         <Comp3 />
 
-        <h1>test</h1>
+        <Footer/>
+        <br></br>
       </div>
     </div>
   );
